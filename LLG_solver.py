37c2a011a1b0f262913a935a7b1b0a5598c2dc79
Alexandra_B <balakshina.ad@edu.spbstu.ru>
2025-06-30 02:39:39 +0300

    Changed logic to be able to count combined cases

--- LLG_solver.py
+++ LLG_solver.py
@@ -4,20 +4,108 @@ from abc import ABC, abstractmethod
 from typing import List, Tuple
 
 
+class Effect(ABC):
+    """
+    Abstract base class for magnetic effects.
+    """
+    @abstractmethod
+    def compute_effective_field(self, m: np.ndarray, Ms: float) -> np.ndarray:
+        """
+        Compute the effective field (must be implemented by subclasses)
+        """
+        pass
+
+
+class AnisotropyEffect(Effect):
+    def __init__(self, K1: float = 4.2e5, K2: float = 1.5e5):
+        """
+        Parameters:
+        ----------
+        K1, K2 : float
+           Cubic anisotropy constant [erg/cc] (default corresponds to Fe)
+        """
+        self.K1 = K1
+        self.K2 = K2
+
+    def compute_effective_field(self, m: np.ndarray, Ms: float) -> np.ndarray:
+        """
+        Compute the dimensionless effective field from cubic anisotropy.
+        h_eff[i] = H_eff[i]/M_s = - 1/M_s^2 dF/dm[i]
+
+        Parameters:
+        ----------
+        m : Magnetization vector (normalized)
+        Ms : Saturation magnetization [emu/cc]
+        Returns:
+        -----------
+        h_eff : Effective field vector
+        """
+        m1, m2, m3 = m
+        h_eff = np.zeros(3)
+        m1_sq, m2_sq, m3_sq = m1 * m1, m2 * m2, m3 * m3
+        Ms_sq = Ms * Ms
+
+        h_eff[0] = -2 * m1 * (self.K1 * (m2_sq + m3_sq) + self.K2 * m2_sq * m3_sq) / Ms_sq
+        h_eff[1] = -2 * m2 * (self.K1 * (m1_sq + m3_sq) + self.K2 * m1_sq * m3_sq) / Ms_sq
+        h_eff[2] = -2 * m3 * (self.K1 * (m1_sq + m2_sq) + self.K2 * m1_sq * m2_sq) / Ms_sq
+
+        return h_eff
+
+
+class ExternalFieldEffect(Effect):
+    def __init__(self, H_ext: np.ndarray):
+        """
+        Parameters:
+        ----------
+        H_ext : np.ndarray
+            External field value [G]
+        """
+        self.H_ext = H_ext
+        # Directions to compare results to
+        print("H_ext direction: ", H_ext / np.linalg.norm(H_ext))
+
+    def compute_effective_field(self, m: np.ndarray, Ms: float) -> np.ndarray:
+        """
+        Compute the dimensionless effective field.
+        h_eff[i] = H_eff[i]/M_s = - 1/M_s^2 dF/dm[i] = H_ext / M_s
+        """
+        return self.H_ext / Ms
+
+
+class DemagFieldEffect(Effect):
+    def __init__(self, N: np.ndarray):
+        """
+        Parameters:
+        ----------
+        N : np.ndarray
+            Demagnetizing factors [dimensionless], sum(N[i]) = 1
+        """
+        self.N = N
+
+    def compute_effective_field(self, m: np.ndarray, Ms: float) -> np.ndarray:
+        """
+        Compute the dimensionless effective field from demagnetization.
+        h_eff[i] = H_eff[i]/M_s = - 1/M_s^2 dF/dm[i] = - 4 * pi * N[i] * m[i]
+        """
+        return -4 * np.pi * self.N * m
+
+
 class LLGSolver(ABC):
     """
-    Abstract class with Landau-Lifshitz-Gilbert (LLG) equation solver.
-
+    Landau-Lifshitz-Gilbert (LLG) equation solver.
+    Uses composition of Effect objects to compute the total effective field.
     Uses normalized quantities:
     - tau = gamma * M_s * t  (dimensionless time)
     - m = M / M_s            (dimensionless magnetization, euclidian_norm(m) = 1)
     """
-    def __init__(self, alpha: float, gamma: float = 1.76e7, Ms: float = 1707):
+    def __init__(self, effects: List[Effect], alpha: float, gamma: float = 1.76e7, Ms: float = 1707):
         """
         Initialize model parameters.
 
         Parameters:
         ----------
+        effects: List[Effect]
+            List of effects that occur in this configuration (anisotropy, demagnetization field, external field).
         alpha : float
             Dimensionless damping constant
         gamma : float, default=1.76e7
@@ -25,16 +113,19 @@ class LLGSolver(ABC):
         Ms : float, default=1707
             Saturation magnetization [emu/cc] (default corresponds to Fe in room temp.)
         """
+        self.effects = effects
         self.gamma = gamma
         self.alpha = alpha
         self.Ms = Ms
 
-    @abstractmethod
     def compute_effective_field(self, m: np.ndarray) -> np.ndarray:
         """
-        Compute the effective field (must be implemented by subclasses)
+        Compute the total effective field by summing contributions from all effects.
         """
-        pass
+        h_eff = np.zeros(3)
+        for effect in self.effects:
+            h_eff += effect.compute_effective_field(m, self.Ms)
+        return h_eff
 
     def f(self, m: np.ndarray, h_eff: np.ndarray) -> np.ndarray:
         """
@@ -165,89 +256,3 @@ class LLGSolver(ABC):
             plt.tight_layout()
 
             plt.show()
-
-
-class AnisotropyLLGSolver(LLGSolver):
-    """
-    An inherited class that only takes into account the effects of anisotropy.
-    """
-    def __init__(self, K1: float = 4.2e5, K2: float = 1.5e5, **kwargs):
-        """
-        Parameters:
-        ----------
-        K1, K2 : float
-            Cubic anisotropy constant [erg/cc] (default corresponds to Fe)
-        """
-        super().__init__(**kwargs)
-        self.K1 = K1
-        self.K2 = K2
-        self.Ms_sq = self.Ms * self.Ms  # Precomputed value for optimization
-
-    def compute_effective_field(self, m: np.ndarray) -> np.ndarray:
-        """
-        Compute the dimensionless effective field from cubic anisotropy.
-        h_eff[i] = H_eff[i]/M_s = - 1/M_s^2 dF/dm[i]
-
-        Parameters:
-        ----------
-        m : Magnetization vector (normalized)
-
-        Returns:
-        -----------
-        h_eff : Effective field vector
-        """
-        m1, m2, m3 = m
-        h_eff = np.zeros(3)
-        m1_sq, m2_sq, m3_sq = m1 * m1, m2 * m2, m3 * m3
-
-        h_eff[0] = -2 * m1 * (self.K1 * (m2_sq + m3_sq) + self.K2 * m2_sq * m3_sq) / self.Ms_sq
-        h_eff[1] = -2 * m2 * (self.K1 * (m1_sq + m3_sq) + self.K2 * m1_sq * m3_sq) / self.Ms_sq
-        h_eff[2] = -2 * m3 * (self.K1 * (m1_sq + m2_sq) + self.K2 * m1_sq * m2_sq) / self.Ms_sq
-
-        return h_eff
-
-
-class ExternalFieldLLGSolver(LLGSolver):
-    """
-    An inherited class that only takes into account the influence of external field.
-    """
-    def __init__(self, H_ext: np.ndarray, **kwargs):
-        """
-        Parameters:
-        ----------
-        H_ext : np.ndarray
-            External field value [G]
-        """
-        super().__init__(**kwargs)
-        self.H_ext = H_ext
-        # Directions to compare results to
-        print("H_ext direction: ", H_ext / np.linalg.norm(H_ext))
-
-    def compute_effective_field(self, m: np.ndarray) -> np.ndarray:
-        """
-        Compute the dimensionless effective field.
-        h_eff[i] = H_eff[i]/M_s = - 1/M_s^2 dF/dm[i] = H_ext / M_s
-        """
-        return self.H_ext / self.Ms
-
-
-class DemagFieldLLGSolver(LLGSolver):
-    """
-    An inherited class that only takes into account the influence demagnetization field.
-    """
-    def __init__(self, N : np.ndarray, **kwargs):
-        """
-        Parameters:
-        ----------
-        N : np.ndarray
-            Demagnetizing factors [dimensionless], sum(N[i]) = 1
-        """
-        super().__init__(**kwargs)
-        self.N = N
-
-    def compute_effective_field(self, m: np.ndarray) -> np.ndarray:
-        """
-        Compute the dimensionless effective field from demagnetization.
-        h_eff[i] = H_eff[i]/M_s = - 1/M_s^2 dF/dm[i] = - 4 * pi * N[i] * m[i]
-        """
-        return -4 * np.pi * self.N * m
--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import numpy as np
-from LLG_solver import LLGSolver, ExternalFieldLLGSolver, DemagFieldLLGSolver, AnisotropyLLGSolver
+from LLG_solver import LLGSolver, AnisotropyEffect, ExternalFieldEffect, DemagFieldEffect
 
 
 class Experiment:
     """
     Handles execution and visualization of experiment.
     """
-    def __init__(self, name, solver_class, solver_params, m0, t_max=1e-8, dt=1e-12):
+    def __init__(self, name, effects, solver_params, m0, t_max=1e-8, dt=1e-12):
         self.name = name
-        self.solver = solver_class(**solver_params)
+        self.effects = effects
+        self.solver_params = solver_params
         self.m0 = m0
         self.t_max = t_max
         self.dt = dt
 
     def run(self):
         print("\n", self.name)
-        print(f"Using t_max = {self.t_max:.2e} s, alpha = {self.solver.alpha}")
-        self.tau_points, self.m_points = self.solver.solve(self.m0, self.t_max, self.dt)
+        solver = LLGSolver(
+            effects=self.effects,
+            **self.solver_params
+        )
+        print(f"Using t_max = {self.t_max:.2e} s, alpha = {solver.alpha}")
+        self.tau_points, self.m_points = solver.solve(self.m0, self.t_max, self.dt)
         print(f"Final magnetization: {self.m_points[-1]}")
 
     def plot(self):
@@ -24,37 +29,53 @@ class Experiment:
 
 
 if __name__ == "__main__":
-    # Initial position of the magnetization vector (randomly chosen)
+    # Initial position of the magnetization vector
     theta = np.deg2rad(30)
     m0 = np.array([np.cos(theta), 0, np.sin(theta)])  # normalized
     print("Initial point: ", m0)
 
-    # Initialize solvers with different parameters
+    # Define common solver parameters
+    base_params = {
+        "alpha": 0.1,
+        "gamma": 1.76e7,
+        "Ms": 1707
+    }
+
+    # Initialize experiments with different effect combinations
     experiments = [
-        Experiment("External Field", ExternalFieldLLGSolver,
-                   {"H_ext": np.array([800, 250, 1000]), "alpha": 0.1}, m0),
-        Experiment("Demagnetization field (Cylinder along Z)", DemagFieldLLGSolver,
-                   {"N": np.array([0.5, 0.5, 0.0]), "alpha": 0.01}, m0, t_max=5e-9),
-        Experiment("Anisotropy", AnisotropyLLGSolver,
-                   {"alpha": 0.1}, m0)
+        Experiment(
+            "External Field Only",
+            effects=[ExternalFieldEffect(H_ext=np.array([800, 250, 1000]))],
+            solver_params={**base_params},
+            m0=m0
+        ),
+        Experiment(
+            "Demagnetization Field (Cylinder along Z)",
+            effects=[DemagFieldEffect(N=np.array([0.5, 0.5, 0.0]))],
+            solver_params={**base_params, "alpha": 0.01},
+            m0=m0,
+            t_max=5e-9
+        ),
+        Experiment(
+            "Anisotropy Only",
+            effects=[AnisotropyEffect(K1=4.2e5, K2=1.5e5)],
+            solver_params={**base_params},
+            m0=m0
+        ),
+        Experiment(
+            "Combined Effects",
+            effects=[
+                AnisotropyEffect(K1=4.2e5, K2=1.5e5),
+                ExternalFieldEffect(H_ext=np.array([800, 250, 1000])),
+                DemagFieldEffect(N=np.array([0.1, 0.1, 0.8]))
+            ],
+            solver_params={**base_params, "alpha": 0.1},
+            m0=m0,
+            t_max=1e-9
+        )
     ]
 
+    # Run all experiments
     for exp in experiments:
-        exp.run()
-        exp.plot()
-
-    # Study of dependence on alpha
-    dif_alpha_exp = [
-        Experiment("Demagnetization field (Plane XY)", DemagFieldLLGSolver,
-                   {"N": np.array([0.0, 0.0, 1.0]), "alpha": 0.1}, m0, t_max=3e-10),
-        Experiment("Demagnetization field (Plane XY)", DemagFieldLLGSolver,
-                   {"N": np.array([0.0, 0.0, 1.0]), "alpha": 0.01}, m0, t_max=3e-9),
-        Experiment("Demagnetization field (Plane XY)", DemagFieldLLGSolver,
-                   {"N": np.array([0.0, 0.0, 1.0]), "alpha": 0.001}, m0, t_max=3e-8),
-        Experiment("Demagnetization field (Plane XY)", DemagFieldLLGSolver,
-                   {"N": np.array([0.0, 0.0, 1.0]), "alpha": 0.0001}, m0, t_max=3e-7),
-    ]
-
-    for exp in dif_alpha_exp:
         exp.run()
         exp.plot()
